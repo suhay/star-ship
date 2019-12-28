e6157ed8a913b8311fa47643379c47091599fedb
Matt Suhay <matthew.suhay@gmail.com>
2019-12-28 09:26:03 -0600

    Setting up based client with test quests

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# star-ship
+# Star Ship
 You are the AI core of an ancient star ship, floating through the void.
+
+Client written with React, backend handled by https://github.com/suhay/quest-log and https://github.com/suhay/sandwich-shop
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const webpack = require(`webpack`)
+
+module.exports = {
+  entry: `./src/index.js`,
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: [`babel-loader`],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [`*`, `.js`],
+  },
+  output: {
+    path: `${__dirname}/dist`,
+    publicPath: `/`,
+    filename: `bundle.js`,
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  devServer: {
+    contentBase: `./dist`,
+    hot: true,
+  },
+}
